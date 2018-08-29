5e8b30fa4287834aa318c7d8fa7c5f856e9dab56
007dahai007 <2405644507@qq.com>
2018-08-29 16:28:05 +0800

    first commit

---
+++ app/src/main/java/com/dahai/demo/TwoActivity.java
@@ -0,0 +1,21 @@
+package com.dahai.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * File: TwoActivity.java
+ * 作者: 大海
+ * 创建日期: 2018/8/29 0029 16:10
+ * 描述：
+ */
+public class TwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+
+    }
+}
